66faf3dcba4391a7e0af124255739e8cb0e21c2a
Gonçalo Cunha <goncalo.cunha@ua.pt>
2024-04-20 00:36:01 +0100

    folder organization

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import '/style.css';
+import '/css/style.css';
 import * as THREE from 'three';
 import vertexShader from '/shaders/vertexShader.glsl'
 import fragmentShader from '/shaders/fragmentShader.glsl'
